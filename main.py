f2aa166462ea0a656d5a72d172038f148832d3bd
Tatavrishi <tatavrishi@gmail.com>
2023-09-24 16:00:56 +0530

    Updated DB and model files

---
+++ main.py
@@ -0,0 +1,44 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+#App object
+app = FastAPI()
+
+
+
+origins = ['https://localhost:8000']
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+@app.get("/")
+async def read_root():
+    return {"Hello": "World"}
+
+@app.get("/api/todo")
+async def get_todo():
+    return 1
+
+@app.get("/api/todo{id}")
+async def get_todo_by_id(id):
+    return 1
+
+@app.post("/api/todo")
+async def post_todo(todo):
+    return 1
+
+@app.put("/api/todo{id}")
+async def put_todo(id, data):
+    return 1
+
+@app.delete("/api/todo{id}")
+async def delete_todo(id):
+    return 1
+
+
+
